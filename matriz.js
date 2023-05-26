0e5c24dd85599d41d0a54e7a681f66db57412170
zIthalo <ithalowillian@gmail.com>
2023-05-25 23:52:31 -0300

    new update

---
+++ matriz.js
@@ -0,0 +1,12 @@
+let matriz = [];
+
+// Preenchendo a matriz com o número 1
+for (let i = 0; i < 3; i++) {
+  matriz[i] = [];
+
+  for (let j = 0; j < 4; j++) {
+    matriz[i][j] = 1;
+  }
+}
+
+console.log(matriz);
